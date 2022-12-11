de7922ac17e8b2e0210742f3085e03a7ee0cfe65
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2022-12-11 20:14:44 +0200

    update unused vars

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,30 +1,20 @@
-import PropTypes from 'prop-types';
 import Input from 'components/base/Input.styled';
-import { useSelector, useDispatch } from "react-redux";
-import { getFilter } from 'redux/selectors';
+import { useDispatch } from 'react-redux';
 import { setFilter } from 'redux/filterSlice';
 
-
-const Filter = ({ value, onChange }) => {
+const Filter = () => {
   const dispatch = useDispatch();
-  // const filter = useSelector(getFilter);
 
   const handleFilterChange = e => dispatch(setFilter(e.currentTarget.value));
 
-  // const handleFilterChange = e => console.log(e.target.value);
-
   return (
-  <>
-    <span>Find contacts by name</span>
-    <label>
-      <Input type="text" name="filter" onChange={handleFilterChange} />
-    </label>
-  </>)
+    <>
+      <span>Find contacts by name</span>
+      <label>
+        <Input type="text" name="filter" onChange={handleFilterChange} />
+      </label>
+    </>
+  );
 };
-
-// Filter.propTypes = {
-//   value: PropTypes.string.isRequired,
-//   onChange: PropTypes.func.isRequired,
-// };
 
 export default Filter;
